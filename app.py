f94e9ba6db8c1f6e3f6735bba1092f4f866a7484
Manasi Mhatre <mr75mhatre2006@gmail.com>
2025-04-17 02:23:51 +0530

    SQLi detection model done

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask, jsonify, request
 import logging
 import requests
 from flask_cors import CORS
-
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 app = Flask(__name__)
 
 CORS(app) 
+
+model = AutoModelForSequenceClassification.from_pretrained("sqli_model")
+tokenizer = AutoTokenizer.from_pretrained("sqli_model")
+
+
+# sample_input = "SELECT * FROM users WHERE username = 'admin' --"
+# inputs = tokenizer(sample_input, return_tensors="pt", truncation=True, padding=True)
+# with torch.no_grad():
+#     outputs = model(**inputs)
+#     logits = outputs.logits
+#     predicted_class = torch.argmax(logits, dim=1).item()
+
+# if predicted_class == 1:
+#     print("Detected: SQL Injection")
+# else:
+#     print("Clean Query")
+
 
 @app.route("/url", methods=["GET", "POST"])
 def getURL():
